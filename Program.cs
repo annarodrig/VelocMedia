5ecaca9d6cc59092cdf61f5f7e326ecf60a0a3ff
= <=>
2022-04-10 16:40:08 -0300

    Finaliza exercicio

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿double d, t, v;
+
+Console.Write("Distância percorrida (m):");
+d = Convert.ToDouble (Console.ReadLine());
+
+Console.Write("Tempo gasto (s):");
+t = Convert.ToDouble (Console.ReadLine());
+
+v = d / t;
+
+Console.WriteLine($"Velocidade média:{v:N2}m/s");
